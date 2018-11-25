916cfb6832e04becda91ed202bc5bc155cb53aa3
Przemyslaw Biskup <przemyslaw@applover.pl>
2018-11-25 03:48:45 +0100

    commit

--- taco_ios/FamilyViewController.swift
+++ taco_ios/FamilyViewController.swift
@@ -17,6 +17,68 @@ final class FamilyViewController: HeaderViewController {
         return FamilyRouter(source: self)
     }()
     
+    private var lineConstraint: NSLayoutConstraint?
+    
+    private let tableMenuStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 8.0
+        return stackView
+    }()
+    
+    private let personsButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("osoby", for: .normal)
+        button.setTitleColor(AppColors.dark, for: .normal)
+        button.addTarget(self, action: #selector(selectC(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private let targetsButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("cele", for: .normal)
+        button.setTitleColor(AppColors.dark, for: .normal)
+        button.addTarget(self, action: #selector(selectC(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private let activitiesButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("aktywności", for: .normal)
+        button.setTitleColor(AppColors.dark, for: .normal)
+        button.addTarget(self, action: #selector(selectC(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private let budgetButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("budżet", for: .normal)
+        button.setTitleColor(AppColors.dark, for: .normal)
+        button.addTarget(self, action: #selector(selectC(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    private let lineView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = AppColors.dark
+        return view
+    }()
+    
+    private let selectLineView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = AppColors.dark
+        return view
+    }()
+    
     private let tableView = UITableView()
     
     override func viewDidLoad() {
@@ -32,6 +94,14 @@ final class FamilyViewController: HeaderViewController {
         tableView.backgroundColor = .clear
         tableView.register(FamilyCell.self, forCellReuseIdentifier: String(describing: FamilyCell.self))
         setUpConstrains()
+    }
+    
+    @objc private func selectC(_ sender: UIButton) {
+        let x = sender.frame.origin.x + sender.frame.width/2
+        lineConstraint?.constant = x
+        UIView.animate(withDuration: 0.25) { [weak self] in
+            self?.view.layoutIfNeeded()
+        }
     }
 }
 
@@ -64,10 +134,38 @@ extension FamilyViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         // adding views
+        view.addSubview(tableMenuStackView)
+        tableMenuStackView.addArrangedSubview(personsButton)
+        tableMenuStackView.addArrangedSubview(targetsButton)
+        tableMenuStackView.addArrangedSubview(activitiesButton)
+        tableMenuStackView.addArrangedSubview(budgetButton)
+        
+        personsButton.widthAnchor.constraint(equalTo: targetsButton.widthAnchor).isActive = true
+        personsButton.widthAnchor.constraint(equalTo: budgetButton.widthAnchor).isActive = true
+        
+        view.addSubview(selectLineView)
+        view.addSubview(lineView)
         view.addSubview(tableView)
         
         // adding constraints
-        tableView.topAnchor.constraint(equalTo: backImageView.bottomAnchor).isActive = true
+        tableMenuStackView.topAnchor.constraint(equalTo: backImageView.bottomAnchor, constant: 8).isActive = true
+        tableMenuStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 6).isActive = true
+        tableMenuStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -6).isActive = true
+        tableMenuStackView.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        
+        selectLineView.topAnchor.constraint(equalTo: tableMenuStackView.bottomAnchor, constant: 8).isActive = true
+        selectLineView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        selectLineView.heightAnchor.constraint(equalToConstant: 1.5).isActive = true
+        lineConstraint = selectLineView.centerXAnchor.constraint(equalTo: tableMenuStackView.leadingAnchor)
+        lineConstraint?.constant = personsButton.frame.origin.x + personsButton.frame.width/2
+        lineConstraint?.isActive = true
+        
+        lineView.topAnchor.constraint(equalTo: tableMenuStackView.bottomAnchor, constant: 8).isActive = true
+        lineView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 8).isActive = true
+        lineView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -8).isActive = true
+        lineView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: selectLineView.bottomAnchor, constant: 4).isActive = true
         tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
